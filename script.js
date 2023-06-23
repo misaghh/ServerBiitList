1505cd9a708afe56d38ae42edbd19c987a3f3cc6
Sepehr Saljooghi <ssaljooghi5@gmail.com>
2023-06-23 16:12:39 -0400

    pasted here

---
+++ script.js
@@ -0,0 +1,25 @@
+// import fetch from 'node-fetch';
+
+// const response = await fetch('https://publicapi.battlebit.cloud/Servers/GetServerList');
+
+
+
+// fetch('https://reqbin.com/echo/get/json', {
+//     method: 'GET',
+//     headers: {
+//         'Accept': 'application/json',
+//     },
+// })
+//    .then(response => response.json())
+//    .then(response => console.log(JSON.stringify(response)))
+
+
+// function getServerList() {
+//     fetch('https://publicapi.battlebit.cloud/Servers/GetServerList').then((Response) => Response.json()).then((data) => {
+//         console.log(data);
+//     }).catch((err) => {
+//         console.log
+//     }
+// }
+
+// getServerList()
